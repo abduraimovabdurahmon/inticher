7cde0a57e2a291565aaa7f53e538719c2df0583d
= <=>
2023-03-11 17:09:05 +0500

    sdfg

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const {Telegraf, Scenes: {Stage}} = require('telegraf');
 const Localsession = require('telegraf-session-local');
 const cron = require("node-cron");
+
+// dotenv
 require('dotenv').config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
 
 
 
-bot.launch();
+
+
+
+
+
+// bot.launch();
