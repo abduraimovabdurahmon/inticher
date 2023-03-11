c5ed7e149f8865b35f17f73f434ccf31711abc35
= <=>
2023-03-11 14:40:31 +0500

    botni ishlatib ko'rdim))

---
+++ index.js
@@ -0,0 +1,12 @@
+const {Telegraf, Scenes: {Stage}} = require('telegraf');
+const Localsession = require('telegraf-session-local');
+const cron = require("node-cron");
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.on('message', (ctx) => {
+    console.log(ctx.message.text);
+})
+
+bot.launch();
